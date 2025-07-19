d7bb5841311e0879f4bc1891c41e29bd3a2eecd7
mc-sss <3459275094@qq.com>
2025-07-19 10:25:12 +0800

    解决清单问题，优化了css

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// document.getElementById('trigger').addEventListener('click', function() {
-//     var hiddenText = document.getElementById('hiddenText');
-//     hiddenText.classList.toggle('hidden');
-//     var triggerText = document.getElementById('trigger');
-//     var text = triggerText.textContent;
-//     if (text === '♦点击展开文本') {
-//         triggerText.textContent = '♦点击收起文本'
-//     } else {
-//         triggerText.textContent = '♦点击展开文本'
-//     }
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     // 获取所有带子菜单的项
     const menuItems = document.querySelectorAll('.with-submenu');
